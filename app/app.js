293524c45c17c48cd301fbd0001be5d327ec482d
jsheperd <juhasz.janos.flutemaker@gmail.com>
2016-01-16 15:03:06 +0100

    constant

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
-var app = angular.module('app');
+var app = angular.module('app', []);
 
+app.constant('config', {
+  'base' : 'http://localhost/ajax_bus/' 
+});
 
-app.service('service', function() {
+app.service('service', function($http, config) {
   
 });
 
-app.controller('controller', function($http, service) {
-
+app.controller('controller', function($scope, service, config) {
+  $scope.name = config.base;
 
 });
